f401d8daea1da7ed8a1899b8edf627ecb593bf7f
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-02-24 03:21:46 +0200

    Add instance/class/method weaving.

--- src/aspectlib.py
+++ src/aspectlib.py
@@ -1,5 +1,16 @@
-from types import GeneratorType, FunctionType, ModuleType, MethodType, UnboundMethodType, ClassType, InstanceType
+from __future__ import print_function
+
 import sys
+from collections import deque
+from contextlib import nested
+from functools import wraps
+from logging import getLogger
+from types import ClassType
+from types import FunctionType
+from types import GeneratorType
+from types import MethodType
+
+logger = getLogger(__name__)
 
 
 class proceed(object):  # pylint: disable=C0103
@@ -19,6 +30,7 @@ class aspect(object):  # pylint: disable=C0103
         self.advise_function = advise_function
 
     def decorate(self, cutpoint_function):
+        @wraps(cutpoint_function)
         def advice_wrapper(*args, **kwargs):
             advisor = self.advise_function(*args, **kwargs)
             if not isinstance(advisor, GeneratorType):
@@ -30,18 +42,19 @@ class aspect(object):  # pylint: disable=C0103
                         args = advice.args
                         kwargs = advice.kwargs
                     try:
-                        advice = advisor.send(cutpoint_function(*args, **kwargs))
+                        result = cutpoint_function(*args, **kwargs)
                     except Exception:
                         advice = advisor.throw(*sys.exc_info())
+                    else:
+                        try:
+                            advice = advisor.send(result)
+                        except StopIteration:
+                            return result
                 elif advice is return_:
                     return
                 elif isinstance(advice, return_):
                     return advice.value
         return advice_wrapper
-
-
-class Weaver(object):
-    pass
 
 
 class Entanglement(object):  # pylint: disable=C0103
@@ -55,30 +68,52 @@ class Entanglement(object):  # pylint: disable=C0103
         self.rollback()
 
 
-weaver_registry = {
-    FunctionType, ModuleType, MethodType, UnboundMethodType, ClassType, InstanceType
-}
-
-
-def weave(target, aspect_or_factory, *factory_args, **factory_kwargs):
-    if isinstance(aspect_or_factory, aspect):
-        aspect_inst = aspect_or_factory
-    else:
-        aspect_inst = aspect_or_factory(*factory_args, **factory_kwargs)
-    assert isinstance(aspect_inst, aspect), '%s did not return an Aspect instance, it return %s.' % (aspect_or_factory, aspect)
-
+def weave(target, aspect_inst, skip_magic_methods=True, skip_subclasses=False):
+    assert isinstance(aspect_inst, aspect), '%s must be an `aspect` instance.' % (aspect_inst)
     if isinstance(target, FunctionType):
+        logger.debug("Weaving %r as plain function.", target)
         mod = __import__(target.__module__)
         name = target.__name__
         assert getattr(mod, name) is target
         setattr(mod, name, aspect_inst.decorate(target))
         return Entanglement(lambda: setattr(mod, name, target))
-    elif isinstance(target, UnboundMethodType):
-        klass = target.im_class
-        name = target.__name__
-        func = klass.__dict__[name]
-        setattr(klass, name, aspect_inst.decorate(target))
-        return Entanglement(lambda: setattr(klass, name, func))
-    elif isinstance(target, ClassType):
-        pass
+    elif isinstance(target, MethodType):
+        if target.im_self:
+            inst = target.im_self
+            name = target.__name__
+            logger.debug("Weaving %r (%s) as instance method.", target, name)
+            func = getattr(inst, name)
+            setattr(inst, name, aspect_inst.decorate(func).__get__(inst, type(inst)))
+            return Entanglement(lambda: delattr(inst, name))
+        else:
+            klass = target.im_class
+            name = target.__name__
+            logger.debug("Weaving %r (%s) as class method.", target, name)
+            func = klass.__dict__[name]
+            setattr(klass, name, aspect_inst.decorate(func))
+            return Entanglement(lambda: setattr(klass, name, func))
+    elif isinstance(target, (type, ClassType)):
+        logger.debug("Weaving %r as class.", target)
+        original = {}
+        for name, func in target.__dict__.items():
+            if skip_magic_methods and name.startswith('__') or name.endswith('__'):
+                continue
+            if callable(func):
+                setattr(target, name, aspect_inst.decorate(func))
+            elif isinstance(func, staticmethod):
+                setattr(target, name, staticmethod(aspect_inst.decorate(func.__func__)))
+            elif isinstance(func, classmethod):
+                setattr(target, name, classmethod(aspect_inst.decorate(func.__func__)))
+            else:
+                continue
+            original[name] = func
 
+        entanglements = [
+            Entanglement(lambda: deque((setattr(target, name, func) for name, func in original.items()), maxlen=0))
+        ]
+        if not skip_subclasses and hasattr(target, '__subclasses__'):
+            for sub in target.__subclasses__():
+                entanglements.append(weave(sub, aspect_inst, skip_magic_methods=skip_magic_methods))
+        return nested(*entanglements)
+    else:
+        raise RuntimeError("Can't weave object %s of type %s" % (target, type(target)))
--- tests/test_aspectlib.py
+++ tests/test_aspectlib.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import unittest
+from contextlib import nested
 
 import aspectlib
 
@@ -98,31 +99,343 @@ class AOPTestCase(unittest.TestCase):
 
     def test_weave_class_meth(self):
         @aspectlib.aspect
-        def aspect(self, foo):
+        def aspect(self, *_):
             self.foo = 'bar'
             yield aspectlib.return_
 
         with aspectlib.weave(TestClass.foobar, aspect):
             inst = TestClass('stuff')
             self.assertEqual(inst.foo, 'bar')
+            inst.foobar()
 
         inst = TestClass('stuff')
         self.assertEqual(inst.foo, 'stuff')
 
     def test_weave_instance_meth(self):
         @aspectlib.aspect
-        def aspect(self, foo):
+        def aspect(self):
             self.foo = 'bar'
             yield aspectlib.return_
 
         inst = TestClass()
-
         with aspectlib.weave(inst.foobar, aspect):
-            inst.__init__('stuff')
+            print(inst.foobar)
+            inst.foobar()
             self.assertEqual(inst.foo, 'bar')
 
-        inst.__init__('stuff')
+        inst.foobar('stuff')
         self.assertEqual(inst.foo, 'stuff')
+
+    def test_weave_class(self):
+        history = []
+
+        @aspectlib.aspect
+        def aspect(*args):
+            history.append(args)
+            args += ':)',
+            yield aspectlib.proceed(*args)
+            yield aspectlib.return_('bar')
+
+        inst = TestClass()
+
+        with aspectlib.weave(TestClass, aspect):
+            inst = TestClass('stuff')
+            self.assertEqual(inst.foo, 'stuff')
+            self.assertEqual(inst.bar, ':)')
+            self.assertEqual(inst.inst, 'bar')
+            self.assertEqual(inst.klass, 'bar')
+            self.assertEqual(inst.static, 'bar')
+            self.assertEqual(TestClass.foo, 'stuff')
+            self.assertEqual(TestClass.bar, ':)')
+            self.assertEqual(TestClass.static_foobar('stuff'), 'bar')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (TestClass, 'stuff'),
+                ('stuff',),
+                ('stuff',),
+            ])
+            del history[:]
+
+            inst = TestSubClass('stuff')
+            self.assertEqual(inst.sub_foo, 'stuff')
+            self.assertEqual(inst.sub_bar, ':)')
+            self.assertEqual(inst.inst, 'bar')
+            self.assertEqual(inst.klass, 'bar')
+            self.assertEqual(inst.static, 'bar')
+            self.assertEqual(TestSubClass.sub_foo, 'stuff')
+            self.assertEqual(TestSubClass.sub_bar, ':)')
+            self.assertEqual(TestSubClass.static_foobar('stuff'), 'bar')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (TestSubClass, 'stuff'),
+                ('stuff',),
+                ('stuff',),
+            ])
+            del history[:]
+
+            inst = TestSubSubClass('stuff')
+            self.assertEqual(inst.subsub_foo, 'stuff')
+            self.assertEqual(inst.subsub_bar, ':)')
+            self.assertEqual(inst.inst, 'bar')
+            self.assertEqual(inst.klass, 'bar')
+            self.assertEqual(inst.static, 'bar')
+            self.assertEqual(TestSubSubClass.subsub_foo, 'stuff')
+            self.assertEqual(TestSubSubClass.subsub_bar, ':)')
+            self.assertEqual(TestSubSubClass.static_foobar('stuff'), 'bar')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (TestSubSubClass, 'stuff'),
+                ('stuff',),
+                ('stuff',),
+            ])
+            del history[:]
+
+        inst = TestClass('stuff')
+        self.assertEqual(inst.foo, 'stuff')
+        self.assertEqual(inst.bar, None)
+        self.assertEqual(inst.inst, None)
+        self.assertEqual(inst.klass, None)
+        self.assertEqual(inst.static, 'stuff')
+        self.assertEqual(TestClass.foo, 'stuff')
+        self.assertEqual(TestClass.bar, None)
+        self.assertEqual(TestClass.static_foobar('stuff'), 'stuff')
+
+        inst = TestSubClass('stuff')
+        self.assertEqual(inst.sub_foo, 'stuff')
+        self.assertEqual(inst.sub_bar, None)
+        self.assertEqual(inst.inst, None)
+        self.assertEqual(inst.klass, None)
+        self.assertEqual(inst.static, 'substuff')
+        self.assertEqual(TestSubClass.sub_foo, 'stuff')
+        self.assertEqual(TestSubClass.sub_bar, None)
+        self.assertEqual(TestSubClass.static_foobar('stuff'), 'substuff')
+
+        inst = TestSubSubClass('stuff')
+        self.assertEqual(inst.subsub_foo, 'stuff')
+        self.assertEqual(inst.subsub_bar, None)
+        self.assertEqual(inst.inst, None)
+        self.assertEqual(inst.klass, None)
+        self.assertEqual(inst.static, 'subsubstuff')
+        self.assertEqual(TestSubSubClass.subsub_foo, 'stuff')
+        self.assertEqual(TestSubSubClass.subsub_bar, None)
+        self.assertEqual(TestSubSubClass.static_foobar('stuff'), 'subsubstuff')
+
+        self.assertEqual(history, [])
+
+    def test_weave_class_old_style(self):
+        history = []
+
+        @aspectlib.aspect
+        def aspect(*args):
+            history.append(args)
+            args += ':)',
+            yield aspectlib.proceed(*args)
+            yield aspectlib.return_('bar')
+
+        inst = LegacyTestClass()
+
+        with nested(
+            aspectlib.weave(LegacyTestClass, aspect),
+            aspectlib.weave(LegacyTestSubClass, aspect),
+            aspectlib.weave(LegacyTestSubSubClass, aspect)
+        ):
+            inst = LegacyTestClass('stuff')
+            self.assertEqual(inst.foo, 'stuff')
+            self.assertEqual(inst.bar, ':)')
+            self.assertEqual(inst.inst, 'bar')
+            self.assertEqual(inst.klass, 'bar')
+            self.assertEqual(inst.static, 'bar')
+            self.assertEqual(LegacyTestClass.foo, 'stuff')
+            self.assertEqual(LegacyTestClass.bar, ':)')
+            self.assertEqual(LegacyTestClass.static_foobar('stuff'), 'bar')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (LegacyTestClass, 'stuff'),
+                ('stuff',),
+                ('stuff',),
+            ])
+            del history[:]
+
+            inst = LegacyTestSubClass('stuff')
+            self.assertEqual(inst.sub_foo, 'stuff')
+            self.assertEqual(inst.sub_bar, ':)')
+            self.assertEqual(inst.inst, 'bar')
+            self.assertEqual(inst.klass, 'bar')
+            self.assertEqual(inst.static, 'bar')
+            self.assertEqual(LegacyTestSubClass.sub_foo, 'stuff')
+            self.assertEqual(LegacyTestSubClass.sub_bar, ':)')
+            self.assertEqual(LegacyTestSubClass.static_foobar('stuff'), 'bar')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (LegacyTestSubClass, 'stuff'),
+                ('stuff',),
+                ('stuff',),
+            ])
+            del history[:]
+
+            inst = LegacyTestSubSubClass('stuff')
+            self.assertEqual(inst.subsub_foo, 'stuff')
+            self.assertEqual(inst.subsub_bar, ':)')
+            self.assertEqual(inst.inst, 'bar')
+            self.assertEqual(inst.klass, 'bar')
+            self.assertEqual(inst.static, 'bar')
+            self.assertEqual(LegacyTestSubSubClass.subsub_foo, 'stuff')
+            self.assertEqual(LegacyTestSubSubClass.subsub_bar, ':)')
+            self.assertEqual(LegacyTestSubSubClass.static_foobar('stuff'), 'bar')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (LegacyTestSubSubClass, 'stuff'),
+                ('stuff',),
+                ('stuff',),
+            ])
+            del history[:]
+
+        inst = LegacyTestClass('stuff')
+        self.assertEqual(inst.foo, 'stuff')
+        self.assertEqual(inst.bar, None)
+        self.assertEqual(inst.inst, None)
+        self.assertEqual(inst.klass, None)
+        self.assertEqual(inst.static, 'stuff')
+        self.assertEqual(LegacyTestClass.foo, 'stuff')
+        self.assertEqual(LegacyTestClass.bar, None)
+        self.assertEqual(LegacyTestClass.static_foobar('stuff'), 'stuff')
+
+        inst = LegacyTestSubClass('stuff')
+        self.assertEqual(inst.sub_foo, 'stuff')
+        self.assertEqual(inst.sub_bar, None)
+        self.assertEqual(inst.inst, None)
+        self.assertEqual(inst.klass, None)
+        self.assertEqual(inst.static, 'substuff')
+        self.assertEqual(LegacyTestSubClass.sub_foo, 'stuff')
+        self.assertEqual(LegacyTestSubClass.sub_bar, None)
+        self.assertEqual(LegacyTestSubClass.static_foobar('stuff'), 'substuff')
+
+        inst = LegacyTestSubSubClass('stuff')
+        self.assertEqual(inst.subsub_foo, 'stuff')
+        self.assertEqual(inst.subsub_bar, None)
+        self.assertEqual(inst.inst, None)
+        self.assertEqual(inst.klass, None)
+        self.assertEqual(inst.static, 'subsubstuff')
+        self.assertEqual(LegacyTestSubSubClass.subsub_foo, 'stuff')
+        self.assertEqual(LegacyTestSubSubClass.subsub_bar, None)
+        self.assertEqual(LegacyTestSubSubClass.static_foobar('stuff'), 'subsubstuff')
+
+        self.assertEqual(history, [])
+
+    def test_weave_class_all_magic(self):
+        history = []
+
+        @aspectlib.aspect
+        def aspect(*args):
+            history.append(args)
+            yield aspectlib.proceed
+
+        inst = TestClass()
+
+        with aspectlib.weave(TestClass, aspect, skip_magic_methods=False):
+            inst = TestClass('stuff')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (TestClass, 'stuff'),
+                ('stuff',),
+            ])
+            del history[:]
+
+            inst = TestSubClass('stuff')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (TestSubClass, 'stuff'),
+                ('stuff',),
+            ])
+            del history[:]
+
+            inst = TestSubSubClass('stuff')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (TestSubSubClass, 'stuff'),
+                ('stuff',),
+            ])
+            del history[:]
+
+        inst = TestClass('stuff')
+        inst = TestSubClass('stuff')
+        inst = TestSubSubClass('stuff')
+
+        self.assertEqual(history, [])
+
+    def test_weave_class_old_style_all_magic(self):
+        history = []
+
+        @aspectlib.aspect
+        def aspect(*args):
+            history.append(args)
+            yield aspectlib.proceed
+
+        inst = LegacyTestClass()
+
+        with nested(
+            aspectlib.weave(LegacyTestClass, aspect, skip_magic_methods=False),
+            aspectlib.weave(LegacyTestSubClass, aspect, skip_magic_methods=False),
+            aspectlib.weave(LegacyTestSubSubClass, aspect, skip_magic_methods=False)
+        ):
+            inst = LegacyTestClass('stuff')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (LegacyTestClass, 'stuff'),
+                ('stuff',),
+            ])
+            del history[:]
+
+            inst = LegacyTestSubClass('stuff')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (LegacyTestSubClass, 'stuff'),
+                ('stuff',),
+            ])
+            del history[:]
+
+            inst = LegacyTestSubSubClass('stuff')
+            self.assertEqual(history, [
+                (inst, 'stuff'),
+                (LegacyTestSubSubClass, 'stuff'),
+                ('stuff',),
+            ])
+            del history[:]
+
+        inst = LegacyTestClass('stuff')
+        inst = LegacyTestSubClass('stuff')
+        inst = LegacyTestSubSubClass('stuff')
+
+        self.assertEqual(history, [])
+
+    def test_just_proceed(self):
+        @aspectlib.aspect
+        def aspect():
+            yield aspectlib.proceed
+
+        @aspect.decorate
+        def func():
+            return 'stuff'
+
+        self.assertEqual(func(), 'stuff')
+
+    def test_just_proceed_with_error(self):
+        @aspectlib.aspect
+        def aspect():
+            yield aspectlib.proceed
+
+        @aspect.decorate
+        def func():
+            1/0
+
+        self.assertRaises(ZeroDivisionError, func)
+
+    def test_weave_unknown(self):
+        @aspectlib.aspect
+        def aspect():
+            yield aspectlib.proceed
+
+        self.assertRaises(RuntimeError, aspectlib.weave, 1, aspect)
 
 
 def module_func():
@@ -130,15 +443,110 @@ def module_func():
 
 
 class TestClass(object):
-    def __init__(self, foo=None):
-        self.foobar(foo)
+    some = 'attribute'
 
-    def foobar(self, foo):
+    def __init__(self, foo=None):
+        self.inst = self.foobar(foo)
+        self.klass = self.class_foobar(foo)
+        self.static = self.static_foobar(foo)
+        self.other = 123
+
+    def foobar(self, foo, bar=None):
         self.foo = foo
+        self.bar = bar
+
+    @classmethod
+    def class_foobar(cls, foo, bar=None):
+        cls.foo = foo
+        cls.bar = bar
+
+    @staticmethod
+    def static_foobar(foo, bar=None):
+        return bar or foo
+
 
 class TestSubClass(TestClass):
-    def foobar(self, foo):
-        self.boo = foo
+    def foobar(self, foo, bar=None):
+        self.sub_foo = foo
+        self.sub_bar = bar
+
+    @classmethod
+    def class_foobar(cls, foo, bar=None):
+        cls.sub_foo = foo
+        cls.sub_bar = bar
+
+    @staticmethod
+    def static_foobar(foo, bar=None):
+        return 'sub' + (bar or foo)
+
+
+class TestSubSubClass(TestSubClass):
+    def foobar(self, foo, bar=None):
+        self.subsub_foo = foo
+        self.subsub_bar = bar
+
+    @classmethod
+    def class_foobar(cls, foo, bar=None):
+        cls.subsub_foo = foo
+        cls.subsub_bar = bar
+
+    @staticmethod
+    def static_foobar(foo, bar=None):
+        return 'subsub' + (bar or foo)
+
+
+class LegacyTestClass:
+    some = 'attribute'
+
+    def __init__(self, foo=None):
+        self.inst = self.foobar(foo)
+        self.klass = self.class_foobar(foo)
+        self.static = self.static_foobar(foo)
+        self.other = 123
+
+    def foobar(self, foo, bar=None):
+        self.foo = foo
+        self.bar = bar
+
+    @classmethod
+    def class_foobar(cls, foo, bar=None):
+        cls.foo = foo
+        cls.bar = bar
+
+    @staticmethod
+    def static_foobar(foo, bar=None):
+        return bar or foo
+
+
+class LegacyTestSubClass(LegacyTestClass):
+    def foobar(self, foo, bar=None):
+        self.sub_foo = foo
+        self.sub_bar = bar
+
+    @classmethod
+    def class_foobar(cls, foo, bar=None):
+        cls.sub_foo = foo
+        cls.sub_bar = bar
+
+    @staticmethod
+    def static_foobar(foo, bar=None):
+        return 'sub' + (bar or foo)
+
+
+class LegacyTestSubSubClass(LegacyTestSubClass):
+    def foobar(self, foo, bar=None):
+        self.subsub_foo = foo
+        self.subsub_bar = bar
+
+    @classmethod
+    def class_foobar(cls, foo, bar=None):
+        cls.subsub_foo = foo
+        cls.subsub_bar = bar
+
+    @staticmethod
+    def static_foobar(foo, bar=None):
+        return 'subsub' + (bar or foo)
+
 
 if __name__ == '__main__':
     unittest.main()
