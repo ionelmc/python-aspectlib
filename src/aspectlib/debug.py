a4e321ed5eeee2404df4beb317307a3fbcdb1631
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-02-28 03:35:28 +0200

    Cleanup junk from debug module.

--- src/aspectlib/debug.py
+++ src/aspectlib/debug.py
@@ -1,12 +1,10 @@
 import logging
 import os
-import socket
 import sys
 from itertools import islice
 import string
 
 import wrapt
-import aspectlib
 
 logger = logging.getLogger(__name__)
 
@@ -27,8 +25,8 @@ def _make_stack(skip=0, length=6, _sep=os.path.sep):
 ASCII_ONLY = ''.join(i if i in string.printable else '.' for i in (chr(c) for c in range(256)))
 
 
-def log(func=None, stacktrace=10, align_stacktrace=60, arguments=True, result=True, trim_result=False, strip_non_ascii=True, use_logging=False,
-        instance_attributes=(), print_to=sys.stderr):
+def log(func=None, stacktrace=10, align_stacktrace=60, arguments=True, result=True, trim_result=False,
+        strip_non_ascii=True, use_logging=False, instance_attributes=(), print_to=sys.stderr):
     def dump(buf):
         if strip_non_ascii:
             buf = buf.translate(ASCII_ONLY)
@@ -58,7 +56,6 @@ def log(func=None, stacktrace=10, align_stacktrace=60, arguments=True, result=Tr
                         key, val() if call else val
                     ))
             sig = buf = '<%s%s>.%s' % (instance_type.__name__, ''.join(info), name)
-            #print('>>>', instance, sig)
         else:
             sig = buf = func.__name__
         if arguments:
@@ -83,43 +80,3 @@ def log(func=None, stacktrace=10, align_stacktrace=60, arguments=True, result=Tr
         return logged(func)
     else:
         return logged
-
-#_recv = socket._socketobject.recv
-#_recvfrom = socket._socketobject.recvfrom
-#_recv_into = socket._socketobject.recv_into
-#_recvfrom_into = socket._socketobject.recvfrom_into
-#
-#def wrap_meth(self, name):
-#    meth = getattr(self, name)
-#    name = name.upper()
-#    def wrapper(*args):
-#        logging.critical('fd%s.%s%s < %s' % (self.fileno(), name, args, quick_stack(1, 10)))
-#        ret = meth(*args)
-#        logging.critical('  ==> returns: %r', ret)
-#        return ret
-#    return wrapper
-#
-#class DebugSocket(socket._socketobject):
-#    def __init__(self, *args, **kwargs):
-#        super(DebugSocket, self).__init__(*args, **kwargs)
-#        for name in ('recv', 'recvfrom', 'recvfrom_into', 'recv_into'):
-#            setattr(self, name, wrap_meth(self, name))
-#
-#socket.socket = socket.SocketType = socket._socketobject = DebugSocket
-
-if __name__ == '__main__':
-    logging.basicConfig(level='DEBUG')
-    import socket
-    aspectlib.weave(socket.socket, log(use_logging=True, print_to=None, instance_attributes=('fileno()',)), patch_on_init=True, skip_methods=('fileno',))
-    s = socket.socket()
-    s.connect(('127.0.0.1', 80))
-    s.send('GET / HTTP/1.0\r\n\r\n')
-    repr(s.recv(10000))
-    s.close()
-
-    #import test_aspectlib
-    #aspectlib.weave(test_aspectlib.SlotsTestSubClass, log(use_logging=True, print_to=None), patch_on_init=True)
-    #x = test_aspectlib.SlotsTestSubClass('bubu')
-    #x.foobar('tete')
-    #x.class_foobar('tete')
-    #x.static_foobar('tete')
