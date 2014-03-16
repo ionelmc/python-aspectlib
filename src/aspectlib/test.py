f560ed80fdb3a4763b1b8ecaa1474d123729c3c8
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-03-16 18:10:50 +0200

    Add more tests/doc for recording/mocking. Fix bug with bounded record wrapper.

--- src/aspectlib/test.py
+++ src/aspectlib/test.py
@@ -1,8 +1,44 @@
+"""
+This module is designed to be a lightweight, orthogonal and easy to learn replacement for the popular `mock
+<https://pypi.python.org/pypi/mock>`_ framework.
+
+Example usage, suppose you want to test this class:
+
+    >>> class ProductionClass(object):
+    ...     def method(self):
+    ...         return 'stuff'
+    >>> real = ProductionClass()
+
+With :mod:`aspectlib.test`::
+
+    >>> from aspectlib import weave, test
+    >>> patch = weave(real.method, [test.mock(3), test.record(call=True)])
+    >>> real.method(3, 4, 5, key='value')
+    3
+    >>> assert real.method.calls == [(real, (3, 4, 5), {'key': 'value'})]
+
+As a bonus, you have an easy way to rollback all the mess::
+
+    >>> patch.rollback()
+    >>> real.method()
+    'stuff'
+
+With ``mock``::
+
+    >>> from mock import Mock
+    >>> real = ProductionClass()
+    >>> real.method = Mock(return_value=3)
+    >>> real.method(3, 4, 5, key='value')
+    3
+    >>> real.method.assert_called_with(3, 4, 5, key='value')
+
+
+"""
 from collections import namedtuple
 from functools import partial
 from functools import wraps
 
-from wrapt.decorators import FunctionWrapper
+from wrapt.decorators import FunctionWrapper, BoundFunctionWrapper
 
 import aspectlib
 
@@ -28,6 +64,12 @@ def mock(return_value, call=False):
         return mock_wrapper
     return mock_decorator
 
+class BoundRecordingWrapper(BoundFunctionWrapper):
+    calls = None
+
+    def __init__(self, wrapped, instance, wrapper, enabled, binding, parent):
+        super(BoundRecordingWrapper, self).__init__(wrapped, instance, wrapper, enabled, binding, parent)
+        self.calls = parent.calls
 
 class RecordingWrapper(FunctionWrapper):
     """
@@ -38,6 +80,7 @@ class RecordingWrapper(FunctionWrapper):
     :param list calls: Instance to put in the `.calls` attribute.
     """
     calls = None
+    __bound_function_wrapper__ = BoundRecordingWrapper
 
     def __init__(self, wrapped, wrapper, calls):
         super(RecordingWrapper, self).__init__(wrapped, wrapper)
