bc5aeba4154d3fa06aebd32e016decef5949aa9d
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-03-31 22:15:08 +0300

    Add test with tornado coroutines.

--- tests/test_integrations_py2.py
+++ tests/test_integrations_py2.py
@@ -2,11 +2,16 @@ try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
+from datetime import timedelta
+
 import asyncio
+from tornado import gen
+from tornado import ioloop
 
 from aspectlib import debug
 
-def test_decorate_coroutine():
+
+def test_decorate_asyncio_coroutine():
     buf = StringIO()
 
     @asyncio.coroutine
@@ -18,4 +23,19 @@ def test_decorate_coroutine():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(coro())
     output = buf.getvalue()
-    assert 'coro => %r' % 'result'
+    assert 'coro => %r' % 'result' in output
+
+
+def test_decorate_tornado_coroutine():
+    buf = StringIO()
+
+    @gen.coroutine
+    @debug.log(print_to=buf, module=False, stacktrace=2, result_repr=repr)
+    def coro():
+        yield gen.Task(loop.add_timeout, timedelta(microseconds=10))
+        raise StopIteration("result")
+
+    loop = ioloop.IOLoop.current()
+    loop.run_sync(coro)
+    output = buf.getvalue()
+    assert 'coro => %r' % 'result' in output
