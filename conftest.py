ea37f8d49c8d0c8722f5d0fe69655701e058f289
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-04-05 20:59:13 +0300

    Skip any collection on 'pytest' - seems the doctest collector will choke on it.

--- conftest.py
+++ conftest.py
@@ -4,5 +4,5 @@ PY3 = sys.version_info[0] >= 3
 def pytest_ignore_collect(path, config):
     basename = path.basename
 
-    if not PY3 and "py3" in basename or PY3 and "py2" in basename:
+    if not PY3 and "py3" in basename or PY3 and "py2" in basename or 'pytest' in basename:
         return True
