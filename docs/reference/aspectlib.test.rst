5f0b188487f224f63d36b8ffa9e3910f8829b62e
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-04-15 03:53:05 +0300

    Add Story/replay docs.

--- docs/reference/aspectlib.test.rst
+++ docs/reference/aspectlib.test.rst
@@ -6,6 +6,13 @@ aspectlib.test
 
     aspectlib.test.record
     aspectlib.test.mock
+    aspectlib.test.Story
+    aspectlib.test.Replay
 
 .. automodule:: aspectlib.test
-    :members:
+    :members: record, mock, Story
+
+.. autoclass:: Replay(?)
+
+    .. automethod:: missing
+
--- src/aspectlib/test.py
+++ src/aspectlib/test.py
@@ -355,6 +355,95 @@ class EntanglingBase(object):
 
 
 class Story(EntanglingBase):
+    """
+    This a simple yet flexible tool that can do "capture-replay mocking" or "test doubles" [1]_. It leverages
+    ``aspectlib``'s powerful :obj:`weaver <aspectlib.weave>`.
+
+    The ``Story`` allows some testing patterns that are hard to do with other tools:
+
+    * **Proxied mocks**: partially mock `objects` and `modules` so they are called normally if the request is unknown.
+    * **Stubs**: completely mock `objects` and `modules`. Raise errors if the request is unknown.
+
+    The ``Story`` works in two of transactions:
+
+    *   **The story**: You describe what calls you want to mocked. Initially you don't need to write this. Eg:
+
+        ::
+
+            >>> import mymod
+            >>> with Story(mymod) as story:
+            ...     mymod.func('some arg') == 'some result'
+            ...     mymod.func('bad arg') ** ValueError("can't use this")
+
+    *   **The replay**: You run the code uses the interfaces mocked in the `story`. The `replay` always starts from a
+        `story` instance and it runs in two modes:
+
+        *   **stub mode**: The default. Unexpected uses of the mocked interfaces raise errors. Eg:
+
+            ::
+
+                >>> with story.replay():
+                ...     assert mymod.func('some arg') == 'some result'
+                ...     try:
+                ...         mymod.func('bad arg')
+                ...     except ValueError as exc:
+                ...         print("Sucess: exception", exc, "was raised.") # test success
+                ...     else:
+                ...         raise AssertionError("The test expected MyException to be raised")
+                Sucess: exception can't use this was raised.
+
+            While in the **stub mode**, if you try to run ``mymod.function('unexpected arg')`` you will get an
+            ``AssertionError`` as that call wasn't specified in the **story**. Eg:
+
+            ::
+
+                >>> with story.replay():
+                ...     mymod.func('unexpected arg')
+                Traceback (most recent call last):
+                  ...
+                  File "...aspectlib...test.py", line ..., in __call__
+                    raise AssertionError("Unexpected call to %s with args:%s kwargs:%s" % pk)
+                AssertionError: Unexpected call to mymod.func with args:('unexpected arg',) kwargs:frozenset()
+
+        *   `proxy mode`: Unexpected uses are allowed but they are collected for later inspection. Eg:
+
+            ::
+
+                >>> with story.replay(proxy=True) as replay:
+                ...     assert mymod.func('some arg') == 'some result'
+                ...     try:
+                ...         mymod.func('bad arg')
+                ...     except ValueError as exc:
+                ...         print("Sucess: exception", exc, "was raised.") # test success
+                ...     else:
+                ...         raise AssertionError("The test expected MyException to be raised")
+                ...     mymod.func('unexpected arg')
+                Sucess: exception can't use this was raised.
+                Got unexpected arg in the real code !
+
+
+    One way to use this on existing code where it's not clear what needs to be written in the **story** is to start with
+    an empty **story**, run the test code, check the unexpected usage and write the story from that. Eg:
+
+    ::
+
+        >>> print(replay.missing())
+        ### UNEXPECTED CALLS (add these in your story)
+        <BLANKLINE>
+        mymod.func('unexpected arg') == None  # returned
+        <BLANKLINE>
+
+    Now you can just copy that in your story.
+
+    :param target: Targets to weave in the `story`/`replay` transactions.
+    :type target: Same as for :obj:`aspectlib.weave`.
+
+    .. versionchanged:: 0.9.0
+
+        Added in.
+
+    .. [1] http://www.martinfowler.com/bliki/TestDouble.html
+    """
     FunctionWrapper = StoryFunctionWrapper
 
     def __init__(self, target):
@@ -363,6 +452,10 @@ class Story(EntanglingBase):
         self._ids = {}
 
     def replay(self, **options):
+        """
+        :param bool proxy: If ``True`` the `replay` will work in `proxy mode`. Default: ``False`` (`stub mode`).
+        :returns: A :obj:`aspectlib.test.Replay` object.
+        """
         return Replay(self._target, self.calls, **options)
 
 ReplayPair = namedtuple("ReplayPair", ('expected', 'unexpected'))
@@ -416,6 +509,13 @@ def format_calls(calls, prefix=""):
 
 
 class Replay(EntanglingBase):
+    """
+    Object implementing the `replay transaction`.
+
+    This object should be created by :obj:`Story <aspectlib.test.Story>`'s :obj:`replay <aspectlib.test.Story.replay>`
+    method.
+
+    """
     FunctionWrapper = ReplayFunctionWrapper
 
     def __init__(self, target, expected, proxy=False, recurse_lock_factory=allocate_lock):
@@ -426,4 +526,7 @@ class Replay(EntanglingBase):
         self._recurse_lock = recurse_lock_factory()
 
     def missing(self, prefix=True):
+        """
+        Returns a pretty text representation of the unexpected calls (while the `replay` was run in `proxy mode`).
+        """
         return format_calls(self.calls.unexpected, 'UNEXPECTED CALLS (add these in your story)' if prefix else '')
