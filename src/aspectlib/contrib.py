b97e6fd450fdb31b9456bab1626d3e0269b5fabf
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-02-24 03:17:39 +0200

    Improve the retry decorator. Add some tests.

--- src/aspectlib/contrib.py
+++ src/aspectlib/contrib.py
@@ -13,16 +13,37 @@ def retry(func=None, retries=5, backoff=None, exceptions=(IOError, OSError, EOFE
 
     Example::
 
-        >>>
+        >>> should_fail = lambda foo=[1,2,3]: foo and foo.pop()
+        >>> @retry
+        ... def flaky_func():
+        ...     if should_fail():
+        ...         raise OSError('Tough luck!')
+        ...     print("Success!")
+        ...
+        >>> flaky_func()
+        Success!
+
+    If it reaches the retry limit::
+
+        >>> @retry
+        ... def bad_func():
+        ...     raise IOError('Tough luck!')
+        ...
+        >>> bad_func()
+        Traceback (most recent call last):
+        ...
+        OSError: Tough luck!
+
     """
     @Aspect(bind=True)
     def Retry(cutpoint, *args, **kwargs):
         for count in range(retries + 1):
             try:
                 if count and cleanup:
-                    cleanup()
+                    cleanup(*args, **kwargs)
                 yield
-            except exceptions:
+                break
+            except exceptions as exc:
                 if count == retries:
                     raise
                 if not backoff:
@@ -31,10 +52,11 @@ def retry(func=None, retries=5, backoff=None, exceptions=(IOError, OSError, EOFE
                     timeout = backoff
                 else:
                     timeout = backoff(count)
-                logger.exception("%s(%s, %s) raised exception. %s retries left. Sleeping %s secs.",
-                                 cutpoint.__name__, args, kwargs, retries - count, timeout)
+                logger.exception("%s(%s, %s) raised exception %s. %s retries left. Sleeping %s secs.",
+                                 cutpoint.__name__, args, kwargs, exc, retries - count, timeout)
                 sleep(timeout)
     return Retry if func is None else Retry(func)
 
 retry.exponential_backoff = lambda count: 2 ** count
-retry.flat_backoff = lambda count: (1, 5)[count] if count < 2 else 15*2**count
+retry.straight_backoff = lambda count: (1, 2, 5)[count] if count < 3 else 5 * count - 5
+retry.flat_backoff = lambda count: (1, 2, 5, 10, 15, 30, 60)[count if count < 6 else -1]
---
+++ tests/test_contrib.py
@@ -0,0 +1,64 @@
+import time
+
+import pytest
+
+from aspectlib.test import Story, record, mock
+from aspectlib.contrib import retry
+from aspectlib import weave
+
+def flaky_func(arg):
+    if arg:
+        arg.pop()
+        raise IOError('Tough luck!')
+
+
+def test_done_suceess():
+    calls = []
+    @retry
+    def ok_func():
+        calls.append(1)
+
+    ok_func()
+    assert calls == [1]
+
+def test_defaults():
+    calls = []
+    retry(sleep=calls.append)(flaky_func)([None] * 5)
+    assert calls == [0, 0, 0, 0, 0]
+
+
+def test_raises():
+    calls = []
+    pytest.raises(IOError, retry(sleep=calls.append)(flaky_func), [None] * 6)
+    assert calls == [0, 0, 0, 0, 0]
+
+    calls = []
+    pytest.raises(IOError, retry(sleep=calls.append, retries=1)(flaky_func), [None, None])
+    assert calls == [0]
+
+
+def test_backoff():
+    calls = []
+    retry(sleep=calls.append, backoff=1.5)(flaky_func)([None] * 5)
+    assert calls == [1.5, 1.5, 1.5, 1.5, 1.5]
+
+
+def test_backoff_exponential():
+    calls = []
+    retry(sleep=calls.append, retries=10, backoff=retry.exponential_backoff)(flaky_func)([None] * 10)
+    print(calls)
+    assert calls == [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+
+
+def test_backoff_straight():
+    calls = []
+    retry(sleep=calls.append, retries=10, backoff=retry.straight_backoff)(flaky_func)([None] * 10)
+    print(calls)
+    assert calls == [1, 2, 5, 10, 15, 20, 25, 30, 35, 40]
+
+
+def test_backoff_flat():
+    calls = []
+    retry(sleep=calls.append, retries=10, backoff=retry.flat_backoff)(flaky_func)([None] * 10)
+    print(calls)
+    assert calls == [1, 2, 5, 10, 15, 30, 60, 60, 60, 60]
