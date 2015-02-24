08d0f7b66ce84e187b302b61a6834765baf625a2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-02-24 23:46:48 +0200

    Add a test with a class.

--- tests/test_contrib.py
+++ tests/test_contrib.py
@@ -1,10 +1,11 @@
 import time
+from logging import getLogger
 
 import pytest
 
-from aspectlib.test import Story, record, mock
+from aspectlib.test import Story, record, mock, LogCapture
 from aspectlib.contrib import retry
-from aspectlib import weave
+from aspectlib import weave, contrib
 
 def flaky_func(arg):
     if arg:
@@ -62,3 +63,60 @@ def test_backoff_flat():
     retry(sleep=calls.append, retries=10, backoff=retry.flat_backoff)(flaky_func)([None] * 10)
     print(calls)
     assert calls == [1, 2, 5, 10, 15, 30, 60, 60, 60, 60]
+
+
+def test_with_class():
+    logger = getLogger(__name__)
+
+    class Connection(object):
+        count = 0
+
+        @retry
+        def __init__(self, address):
+            self.address = address
+            self.__connect()
+
+        def __connect(self, *_, **__):
+            self.count += 1
+            if self.count % 3:
+                raise OSError("Failed")
+            else:
+                logger.info("connected!")
+
+        @retry(cleanup=__connect)
+        def action(self, arg1, arg2):
+            self.count += 1
+            if self.count % 3 == 0:
+                raise OSError("Failed")
+            else:
+                logger.info("action!")
+
+        def __repr__(self):
+            return "Connection@%s" % self.count
+
+    with LogCapture([logger, contrib.logger]) as logcap:
+        try:
+            conn = Connection("to-something")
+            for i in range(5):
+                conn.action(i, i)
+        finally:
+            for i in logcap.messages:
+                print(i)
+    assert logcap.messages == [
+        ('ERROR', "__init__((Connection@1, 'to-something'), {}) raised exception Failed. 5 retries left. Sleeping 0 secs."),
+        ('ERROR', "__init__((Connection@2, 'to-something'), {}) raised exception Failed. 4 retries left. Sleeping 0 secs."),
+        ('INFO', 'connected!'),
+        ('INFO', 'action!'),
+        ('INFO', 'action!'),
+        ('ERROR', 'action((Connection@6, 2, 2), {}) raised exception Failed. 5 retries left. Sleeping 0 secs.'),
+        ('ERROR', 'action((Connection@7, 2, 2), {}) raised exception Failed. 4 retries left. Sleeping 0 secs.'),
+        ('ERROR', 'action((Connection@8, 2, 2), {}) raised exception Failed. 3 retries left. Sleeping 0 secs.'),
+        ('INFO', 'connected!'),
+        ('INFO', 'action!'),
+        ('INFO', 'action!'),
+        ('ERROR', 'action((Connection@12, 4, 4), {}) raised exception Failed. 5 retries left. Sleeping 0 secs.'),
+        ('ERROR', 'action((Connection@13, 4, 4), {}) raised exception Failed. 4 retries left. Sleeping 0 secs.'),
+        ('ERROR', 'action((Connection@14, 4, 4), {}) raised exception Failed. 3 retries left. Sleeping 0 secs.'),
+        ('INFO', 'connected!'),
+        ('INFO', 'action!'),
+    ]
