0ca9775c610d29a9105f2c4d04b18c7970b6da80
Ionel Cristian Mărieș <contact@ionelmc.ro>
2016-05-10 16:28:00 +0300

    Bump version: 1.4.1 → 1.4.2

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'Aspectlib'
 year = '2014-2016'
 author = 'Ionel Cristian Mărieș'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.4.1'
+version = release = '1.4.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
